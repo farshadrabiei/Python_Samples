f0ca37c9c8011747973b9c9e9760f2d2a577daf2
farshad rabiei <f.rabiei60@hotmail.com>
2020-09-28 08:28:12 +0330

    initial commit

---
+++ utilies/url_utilities.py
@@ -0,0 +1,13 @@
+def load_urls_from_file(file_path: str):
+    # TODO: add code
+    pass
+
+
+def load_page(url: str):
+    # TODO:
+    pass
+
+
+def scrape_page(page_contents: str):
+    # TODO:
+    pass
